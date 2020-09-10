b4222159e2da0f82d59f36db185be01eeaf7745b
Juliana Leon <jleon@homeaway.com>
2020-09-09 19:49:28 -0500

    fix syntax error

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = app => {
   // Your code here
   app.log.info('Yay, the app was loaded!')
 
-  const regex = /\bblacklist\b/i
-  var hasProhibittedTerm = false
+  const regex = RegExp('blacklist');
+  var hasProhibittedTerm = false;
 
   app.on(['issues.opened', 'issues.edited'], async context => {
     app.log.info(context);
@@ -16,17 +16,15 @@ module.exports = app => {
   })
 
   app.on(['pull_request.opened', 'pull_request.edited'], async context => {
-      hasProhibittedTerm = regex.test(context.payload.pull_request.body)
+      hasProhibittedTerm = regex.test(context.payload.pull_request.body);
       // const prComment = context.issue({ body: 'Responding to PR!' })
       // return context.github.issues.createComment(prComment)
 
       // if (hasProhibittedTerm && !context.isBot) {
       if (hasProhibittedTerm) {
-          const params = context.issue({body: 'Hey! {term} is considered problematic language, maybe you could try using {replacement} instead to use more inclusive language? [Here\'s why.](https://confluence.expedia.biz/pages/viewpage.action?pageId=1607388080)'})
-          return context.github.issues.createComment(params)
+          const params = context.issue({body: 'Hey! blacklist is considered problematic language, maybe you could try using replacement instead to use more inclusive language? [Here\'s why.](https://confluence.expedia.biz/pages/viewpage.action?pageId=1607388080)'});
+          return context.github.issues.createComment(params);
       }
-  }
-
   })
 
 
