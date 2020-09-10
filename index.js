d6420ab66b360215874986624ca7a532be83f140
Juliana Leon <jleon@homeaway.com>
2020-09-09 19:36:36 -0500

    Check for problematic PR terms

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ module.exports = app => {
   // Your code here
   app.log.info('Yay, the app was loaded!')
 
+  const regex = /\bblacklist\b/i
+  var hasProhibittedTerm = false
+
   app.on(['issues.opened', 'issues.edited'], async context => {
     app.log.info(context);
     const issueComment = context.issue({ body: 'Thanks for the issue!' })
@@ -13,8 +16,17 @@ module.exports = app => {
   })
 
   app.on(['pull_request.opened', 'pull_request.edited'], async context => {
-      const prComment = context.issue({ body: 'Responding to PR!' })
-      return context.github.issues.createComment(prComment)
+      hasProhibittedTerm = regex.test(context.payload.pull_request.body)
+      // const prComment = context.issue({ body: 'Responding to PR!' })
+      // return context.github.issues.createComment(prComment)
+
+      // if (hasProhibittedTerm && !context.isBot) {
+      if (hasProhibittedTerm) {
+          const params = context.issue({body: 'Hey! {term} is considered problematic language, maybe you could try using {replacement} instead to use more inclusive language? [Here\'s why.](https://confluence.expedia.biz/pages/viewpage.action?pageId=1607388080)'})
+          return context.github.issues.createComment(params)
+      }
+  }
+
   })
 
 
